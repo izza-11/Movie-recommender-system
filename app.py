818c05817e3ddd83df056c36a7d46818742733bf
izza_qureshi <136245332+izza-11@users.noreply.github.com>
2026-01-22 15:36:41 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append((movies.iloc[i[0]].title))
+    return recommended_movies
+
+
+from streamlit.testing.v1.element_tree import Dataframe
+
+movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.title("Movie-Recommender-System")
+select_movie_name = st.selectbox(
+    "How would you like to be contacted?",
+    (movies['title'].values)
+)
+
+
+if st.button("Recommend"):
+    recommendations = recommend(select_movie_name)
+    for i in recommendations:
+       st.write(i)
+
+#
+# st.write("You selected:", select_movie_name)
